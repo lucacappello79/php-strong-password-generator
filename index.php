ea5f5d13479671c768815df972a1c72d24506ae8
lucacappello79 <postt79@gmail.com>
2023-04-19 15:57:33 +0200

    init exercise

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+
+function generatePw($pwLength) {
+    $alphanumericChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=;:<>/?'";
+    $password = "";
+
+    for ($i = 0; $i < $pwLength; $i++) {
+
+        $password .= $alphanumericChars[rand(0, strlen($alphanumericChars) - 1)];
+
+    }
+
+    return $password;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PW GEN</title>
+</head>
+
+
+<body>
+    <h1>Generate Random Password</h1>
+
+    <form action="index.php" method="GET">
+        <label for="pwLength">Choose your password's length </label><br><br>
+        <input type="number" id="pwLength" name="pwLength" min="8" max="16" required><br><br>
+        <input type="submit" value="Create PW"><br><br>
+    </form>
+
+</body>
+
+
+</html>
